a62d209ac4fcd85e955643899432c3332f1fc211
unknown <edus.silva.rosa@gmail.com>
2018-01-16 21:51:30 -0200

    adicionado serviços de salvar e atualizar os produtos

--- src/main/java/org/edu/xyinc/controllers/ProductController.java
+++ src/main/java/org/edu/xyinc/controllers/ProductController.java
@@ -8,12 +8,18 @@ import java.util.List;
 import org.edu.xyinc.domains.Product;
 import org.edu.xyinc.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * @author Eduardo Silva Rosa 
@@ -43,6 +49,22 @@ public class ProductController {
 	public void deleteProduct(@PathVariable Long id){
 		
 		productRepository.delete(id);
+	}
+	
+	@PutMapping(path="/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity updateProduct(@RequestBody Product product){
+		
+		productRepository.save(product);
+		return ResponseEntity.status(HttpStatus.OK).body(null);
+	}
+	
+	@PostMapping(consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity saveProduct(@RequestBody Product product){
+		
+		productRepository.save(product);
+		return ResponseEntity.status(HttpStatus.OK).body(null);
 	}
 
 }
